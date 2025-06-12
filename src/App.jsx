82e3604be2ca47ed758f0eed05b30f321f9f07a0
CristianCabascago95 <sancrilex@gmail.com>
2025-06-12 12:00:47 -0500

    cambio

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import Register from "./components/Register";
 import ResetPassword from "./components/ResetPassword"; 
 import Dashboard from "./pages/Dashboard";
 import Repositorio from "./pages/Repositorio";
-
+import RegistroAcceso from './pages/RegistroAcceso';
+import './App.css'; // Tu archivo CSS global
 
 export default function App() {
   return (
@@ -16,6 +17,7 @@ export default function App() {
         <Route path="/reset-password" element={<ResetPassword />} /> {/* 👈 nueva ruta */}
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/ruta1" element={<Repositorio />} /> {/* <- Aquí se conecta */}
+        <Route path="/ruta2" element={<RegistroAcceso />} /> {/* <-- Nueva ruta */}
 
       </Routes>
     </BrowserRouter>
